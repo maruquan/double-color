e77223d1237bb03d9595b5368977992c31cb696f
maruquan <maruquan_ext@cop.cmsoft.com.cn>
2021-06-01 10:19:05 +0800

    delete file

---
+++ src/main/java/com/ma/doublecolor/DoubleColorApplication.java
@@ -0,0 +1,13 @@
+package com.ma.doublecolor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleColorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleColorApplication.class, args);
+	}
+
+}
