0632609f0b9ea4e41d0420a1fab7a55fb54f0cf4
maruquan <maruquan_ext@cop.cmsoft.com.cn>
2021-06-01 11:39:38 +0800

    get body

---
+++ src/main/java/com/ma/doublecolor/service/StaticMainTest.java
@@ -0,0 +1,8 @@
+package com.ma.doublecolor.service;
+
+public class StaticMainTest {
+
+    public static void main(String[] args) {
+       System.out.println( HttpUtils.doGet("https://zst.cjcp.com.cn/shdd/ssq-lq-100.html"));
+    }
+}
